db5bc584d2fad68532d12737776a455f24c66600
NamrathaRGowda <namrathagowda64@gmail.com>
2017-05-22 06:32:38 +0000

    adding hostname and ipaddress to index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,10 @@ action :install
 end
 
 file 'var/www/html/index.html' do
-content '<h1>Hello World!!</h1>'
+content "<h1>Hello World!!</h1>
+HOSTNAME: #{node['hostname']}
+IPADDRESS: #{node['ipaddress']}
+"
 end
 
 service 'httpd' do
